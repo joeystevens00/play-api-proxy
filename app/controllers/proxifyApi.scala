e8b67ad334e587c270dc0426e874bb7ab82217a0
jstevens00 <joeystevens00@gmail.com>
2017-01-15 19:43:30 -0500

    prevent infinte recursion

--- app/controllers/proxifyApi.scala
+++ app/controllers/proxifyApi.scala
@@ -19,9 +19,9 @@ class Application extends Controller
  		var contentType: String = request.headers.get("Content-Type").getOrElse("")
  		var requestUrlParam: String = request.getQueryString("api-forward-url").getOrElse("")
  		var completeHeaders: Set[String] = Set()
+ 		var error: String ="Incorrect usage. Pass your request URL as api-forward-url in the headers or as a url paramater"
  		if (isBlank(Option(requestUrl)) && isBlank(Option(requestUrlParam))) 
  		{ // If the user didn't give us api-forward-url
- 			var error: String ="Incorrect usage. Pass your request URL as api-forward-url in the headers or as a url paramater"
  			System.out.println(error)
  			BadRequest(error)
  		}
@@ -31,44 +31,49 @@ class Application extends Controller
  				//If the User passed api-forward-url as a URL paramater
  				requestUrl=requestUrlParam
  			}
-
- 			if (contentType.toUpperCase() contains "JSON") 
- 			{	//HANDLING JSON REQUESTS
- 				try 
- 				{          
-    				val json = request.body.asJson.get
-    				val jsonString: String = Json.stringify(json)
-    				//val parsedperson = json.as[Person]
-  		 			System.out.println("Received JSON Request To: " + requestUrl)
- 		 			var result = Http(requestUrl).postData(jsonString)
- 					for (e <- request.headers.keys)  //Process headers
- 					{
- 		 				var key: String = e
- 		 				var value: String = request.headers.get(e).getOrElse("")
- 		 				result=result.header(key, value) //Attach all headers to the request
-					} 
-					Ok(result.asString.body)
- 		 		}
- 		 		catch 
- 		 		{ 
- 		 				case ioe: NoSuchElementException => BadRequest("No such element")
- 		 			  	case e: Exception => BadRequest("Something went wrong: " + e)
- 		 		}
- 		 	} 
- 		 	else 
- 		 	{
-				//HANDLING NON JSON REQUESTS
-				System.out.println("Received NON-JSON Request To: " + requestUrl)
-				var result = Http(requestUrl)
- 				for (e <- request.headers.keys) 
-		 		{
- 				 	var key: String = e
- 				 	var value: String = request.headers.get(e).getOrElse("")
- 		 			result=result.header(key, value)
-				}
- 				Ok(result.asString.body)
-
+ 			if (requestUrl.contains(request.host) && !(requestUrl.contains("getHeaders")) && !(requestUrl.contains("jsonPostTest")))
+ 			{
+ 				BadRequest(error)
  			}
-		}		
+ 			else 
+ 			{
+ 				if (contentType.toUpperCase() contains "JSON") 
+ 				{	//HANDLING JSON REQUESTS
+ 					try 
+ 					{          
+    					val json = request.body.asJson.get
+    					val jsonString: String = Json.stringify(json)
+    					//val parsedperson = json.as[Person]
+  		 				System.out.println("Received JSON Request To: " + requestUrl)
+ 		 				var result = Http(requestUrl).postData(jsonString)
+ 						for (e <- request.headers.keys)  //Process headers
+ 						{
+ 		 					var key: String = e
+ 		 					var value: String = request.headers.get(e).getOrElse("")
+ 		 					result=result.header(key, value) //Attach all headers to the request
+						} 
+						Ok(result.asString.body)
+ 		 			}
+ 		 			catch 
+ 		 			{ 
+ 		 					case ioe: NoSuchElementException => BadRequest("No such element")
+ 		 				  	case e: Exception => BadRequest("Something went wrong: " + e)
+ 		 			}
+ 		 		} 
+ 		 		else 
+ 		 		{
+					//HANDLING NON JSON REQUESTS
+					System.out.println("Received NON-JSON Request To: " + requestUrl)
+					var result = Http(requestUrl)
+ 					for (e <- request.headers.keys) 
+		 			{
+ 					 	var key: String = e
+ 					 	var value: String = request.headers.get(e).getOrElse("")
+ 		 				result=result.header(key, value)
+					}
+ 					Ok(result.asString.body)		
+ 				}
+			}		
+		}
 	}
 }
